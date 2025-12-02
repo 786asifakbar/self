15127510aad76cef6c11cac9e89cbae17a8f6616
Asif Akbar <786asifakbar53@gmail.com>
2025-12-02 13:40:32 +0500

    feat: update cookie-parser to version 1.4.7 and integrate into the application

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import  cors from "cors";
 import cookieParser from "cookie-parser";
 
+
 const app = express();
 
 app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
@@ -12,8 +13,8 @@ app.use(cookieParser());
 
 
 //router declearations
-import userRouter from "./routes/user.routes.js";
-app.use("/api/v1/users", userRouter)
+// import userRouter from "./routes/user.route.js";
+// app.use("/api/v1/users", userRouter)
 
 export default app; 
 
